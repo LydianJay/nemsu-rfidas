d1d4342e0d26ecd527cb3efe7be194d395eb949e
LydianJay <lloydjayedradan@gmail.com>
2024-12-05 10:28:30 +0800

    to be tested

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -15,3 +15,8 @@ $routes->get('/home', 'Home::index');
 
 // In Development
 $routes->get('/indev', 'Maintainance::index');
+
+
+// ESP32
+$routes->post('/insert', 'ESP32::insert');
+$routes->get('/insert', 'ESP32::index');
---
+++ app/Controllers/ESP32.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Controllers;
+
+class ESP32 extends BaseController
+{
+
+   
+
+    public function index()
+    {
+       
+        echo view('header', $this->data);
+        echo view('footer');
+    }
+
+
+    public function insert()
+    {
+        $data   = $this->request->getJSON();
+
+        $date   = date('m-d-Y');
+        $splite = explode('-', $date);
+        $time   = date('H:i');
+        // $q2     = "SELECT COUNT(rfid) as n FROM attendance WHERE rfid = '$data->rfid' AND day = '$day' AND month = '$month' AND year = '$year' ";
+        
+
+        $month  = $splite[0];
+        $day    = $splite[1];
+        $year   = $splite[2];
+
+
+        $isValid = $this->db->table('students')
+        ->selectCount('rfid')
+        ->where('rfid', $data->rfid)
+        ->get()
+        ->getResult();
+
+
+        if($isValid[0]->rfid <= 0)  {
+            return $this->response->setJSON(['status' => -1, 'msg' => 'Unregistered']);
+
+        }
+
+
+        $count = $this->db->table('attendance')
+        ->selectCount('rfid')
+        ->where('rfid', $data->rfid)
+        ->where('day', $day)
+        ->where('month', $month)
+        ->where('year', $year)
+        ->get()
+        ->getResult();
+
+
+
+        if($count[0]->rfid < 2 ) {
+
+            $checkIns = $this->db->table('attendance')
+                ->selectCount('rfid')
+                ->where('rfid', $data->rfid)
+                ->where('day', $day)
+                ->where('month', $month)
+                ->where('year', $year)
+                ->where('type', 1)
+                ->get()
+                ->getResult()[0];
+
+            $type = $checkIns->rfid >= 1 ? 0 : 1;
+            
+            $d = [
+                'rfid'      => $data->rfid,
+                'day'       => $day,
+                'month'     => $month,
+                'year'      => $year,
+                'time'      => $time,
+                'type'      => $type,
+            ];
+
+            $this->db->table('attendance')->insert($d);
+
+
+
+            return $this->response
+            ->setJSON(
+                [
+                    'status' => 'OK', 
+                    'msg' => $type == 0 ? 'In' : 'Out'
+                ]
+            );
+        }
+        else {
+            return $this->response
+            ->setJSON(
+                [
+                    'status' => 'OK',
+                    'msg' => 'Already'
+                ]
+            );        
+        }
+
+
+        return $this->response->setJSON(['count' => $count[0]->rfid]);
+    }
+}
